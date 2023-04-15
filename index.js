17c3f988e9a70a965bb9d73d2be86938274f6ed8
Davy Peres <otaldodavy@gmail.com>
2023-04-15 18:45:43 -0300

    Atualização do site
    Atualização do site

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ var observer = new IntersectionObserver((e) => {
 var hiddenText = document.querySelectorAll('.hidden');
 hiddenText.forEach((el) => observer.observe(el));
 
-function menuAparecer() {
-    const aparecerMenu = document.querySelector('.botao');
-    aparecerMenu.classList.toggle('aparecer')
-}
+var observer2 = new IntersectionObserver((e) => {
+    e.forEach((entry) =>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('workv');
+        } else{
+            entry.target.classList.remove('workv');
+        }
+    });
+});
+
+var hiddenWork = document.querySelectorAll('.work');
+hiddenWork.forEach((el) => observer2.observe(el));
+
+/* Curso */
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
