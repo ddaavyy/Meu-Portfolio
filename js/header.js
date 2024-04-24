c22930164da187fe98b7e2ece6ca565dee6cc861
Davy Peres <otaldodavy@gmail.com>
2024-04-24 16:15:07 -0300

    att

---
+++ js/header.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector('#header')
+    header.classList.toggle('header-cor', window.scrollY > 550 & window.scrollY < 1600)
+
+    var text_header = document.querySelector("#text_header-home")
+    text_header.classList.toggle('text_header-home', window.scrollY > 550 & window.scrollY < 1600)
+
+    var text_header = document.querySelector("#text_header-sobre")
+    text_header.classList.toggle('text_header-sobre', window.scrollY > 550 & window.scrollY < 1600)
+
+    var text_header = document.querySelector("#text_header-hab")
+    text_header.classList.toggle('text_header-hab', window.scrollY > 550 & window.scrollY < 1600)
+
+    var text_header = document.querySelector("#text_header-port")
+    text_header.classList.toggle('text_header-port', window.scrollY > 550 & window.scrollY < 1600)
+
+    var text_header = document.querySelector("#text_header-contato")
+    text_header.classList.toggle('text_header-contato', window.scrollY > 550 & window.scrollY < 1600)
+})
